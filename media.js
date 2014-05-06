ae23c4c3b2c2b6d97ffdde852bb19ab5cb73847f
Ada Rose <ada.edwards@ft.com>
2014-05-06 16:38:10 +0100

    Major rewrite to allow asynchronous state functions and enforcing of teardown setup order.

--- media.js
+++ media.js
@@ -1,7 +1,15 @@
 /* jshint node: true, browser: true */
 
 /**
- * Media Helper
+ * Media Helper rewrite.
+ *
+ * This is a rewrite of media helper to enforce propogation of events from parent to children.
+ * Ensure that teardown of one state is fired before setup of another.
+ *
+ * Usage: Register what events you would like to subscribe to.
+ * These events will also get fired on all the children so if you
+ * reregister on a perchild basis they will get fired twice on the
+ * item and all of it's children.
  *
  * @codingstandard ftlabs-jsv2
  * @copyright The Financial Times Limited [All rights reserved]
@@ -14,26 +22,27 @@
  */
 
 var mm = window.matchMedia;
-var has = Object.prototype.hasOwnProperty;
+var async = require('async');
 
 /**
  * Exports
  */
 
 module.exports = function(module) {
+	var setImmediateId = 0;
+	var setupCallbacks = [];
+	var teardownCallbacks = [];
 
 	module.on('initialize', function(options) {
 		this._media = {};
 
-		var override = options.media;
-		var proto = this.media;
-		var media = override || proto;
+		var media = this.media;
 		var states = expandStates(this.states || {});
 		var state;
 		var name;
 
 		for (var query in media) {
-			if (has.call(media, query)) {
+			if (media.hasOwnProperty(query)) {
 				name = media[query];
 				state = states[name] || {};
 				this._media[name] = {
@@ -50,11 +59,13 @@ module.exports = function(module) {
 		var matcher;
 
 		for (var name in this._media) {
-			if (has.call(this._media, name)) {
+			if (this._media.hasOwnProperty(name)) {
 				state = this._media[name];
 				matcher = state.matcher = mm(state.query);
 				matcher.addListener(state.cb = callback(name));
-				if (matcher.matches) setup(name);
+
+				// Call setup on the current media state.
+				if (matcher.matches) setup(name, function () {});
 			}
 		}
 	});
@@ -64,79 +75,97 @@ module.exports = function(module) {
 		var matcher;
 
 		for (var name in this._media) {
-			if (has.call(this._media, name)) {
+			if (this._media.hasOwnProperty(name)) {
 				state = this._media[name];
 				matcher = state.matcher;
 				matcher.removeListener(state.cb);
-				if (matcher.matches) teardown(name, { fromDOM: false });
+				if (matcher.matches) teardown(name, { fromDOM: false }, function () {});
 			}
 		}
 	});
 
-	function expandStates(states) {
-		var expanded = {};
-		var contents;
-
-		for (var names in states) {
-			if (has.call(states, names)) {
-				contents = states[names];
-				if (~names.indexOf(' ')) {
-					names.split(' ').forEach(copy);
-				} else {
-					expanded[names] = contents;
-				}
-			}
-		}
-
-		function copy(name) {
-			expanded[name] = contents;
-		}
-
-		return expanded;
-	}
-
 	function callback(name) {
 		return function(data) {
+
 			var state = module._media[name];
 
-			// NOTE:WP:Don't do anything if the
-			// matches state has not changed.
-			// This is to work around a strange bug,
-			// whereby sometimes the callback fires
-			// twice.
-			//
-			// I'm not sure if this a browser
-			// bug, or a bug we have caused.
-			if (state.matches === data.matches) return;
-
 			// Either setup or teardown
-			if (data.matches) setup(name);
-			else teardown(name);
+			if (data.matches) {
+				setupCallbacks.push(name);
+			} else {
+				teardownCallbacks.push(name);
+			}
 
-			// Fire an event to allow third
-			// parties to hook into this change.
-			module.fireStatic('statechange');
+			if (!setImmediateId) {
+				setImmediateId = setImmediate(function() {
+					setImmediateId = 0;
+
+					// Run teardown callbacks (if any)
+					var tearDownCallbacksLength = teardownCallbacks.length;
+					async.map(teardownCallbacks, teardown, function (err, results) {
+						teardownCallbacks.slice(tearDownCallbacksLength);
+
+						// Fire an event to allow third
+						// parties to hook into this change.
+						module.fireStatic('statechange');
+
+						// Run Setup callbacks if any.
+						var setupCallbacksLength = setupCallbacks.length;
+						async.map(setupCallbacks, setup, function (err, results) {
+							setupCallbacks.slice(setupCallbacksLength);
+							module.fireStatic('statechangecomplete');
+						});
+					});
+				});
+			}
 
 			// Update the matches state
 			state.matches = data.matches;
 		};
 	}
 
-	function setup(name) {
+
+
+	function setup(name, callback) {
 		module.el.classList.add(name);
-		run('setup', { on: name });
+		run('setup', { on: name }, callback);
 	}
 
-	function teardown(name, options) {
+	function teardown(name, options, callback) {
 		var fromDOM = (!options || options.fromDOM !== false);
 		if (fromDOM) {
 			module.el.classList.remove(name);
 		}
-		run('teardown', { on: name, fromDOM: fromDOM });
+		if (typeof options === "function") callback = options;
+		run('teardown', { on: name, fromDOM: fromDOM }, callback);
 	}
 
-	function run(method, options) {
+	function run(method, options, callback) {
 		var fn = module._media[options.on][method];
-		if (fn) fn.call(module, options);
+		if (fn) fn.call(module, options, callback);
+		else callback();
 	}
 };
+
+// This allows multiple breakpoints to trigger one function.
+function expandStates(states) {
+	var expanded = {};
+	var contents;
+
+	for (var names in states) {
+		if (states.hasOwnProperty(names)) {
+			contents = states[names];
+			if (~names.indexOf(' ')) {
+				names.split(' ').forEach(copy);
+			} else {
+				expanded[names] = contents;
+			}
+		}
+	}
+
+	function copy(name) {
+		expanded[name] = contents;
+	}
+
+	return expanded;
+}
