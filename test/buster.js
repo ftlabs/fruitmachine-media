b18a58cc867ac61eb4c5a4a8a7f05371e21ccad0
Dominic Tobias <dominic.tobias@gmail.com>
2014-11-20 12:45:12 +0000

    Updated buster test es6-promise path

--- test/buster.js
+++ test/buster.js
@@ -5,7 +5,7 @@ config["fruitmachine-media"] = {
 	environment: "browser",
 	sources: [
 		'node_modules/fruitmachine/build/fruitmachine.js',
-		'node_modules/es6-promise/dist/promise-1.0.0.js',
+		'node_modules/es6-promise/dist/es6-promise.js',
 		'node_modules/hogan.js/lib/template.js',
 		'node_modules/hogan.js/lib/compiler.js',
 		'coverage/build/test.js'
